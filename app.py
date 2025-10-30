39b15c428acec75fce1b3552420a45a04c31a599
Mathew <mathewkariuki2005@gmail.com>
2025-10-30 23:25:30 +0300

    changed cors

--- app.py
+++ app.py
@@ -51,7 +51,10 @@ def create_app():
     app.register_blueprint(club_bp, url_prefix='/api')
     app.register_blueprint(debug_bp, url_prefix='/api/debug')
     
-    CORS(app,support_credentials=True)
+    CORS(app,support_credentials=True,resources={r"/*": {"origins": [
+    "https://eventhub-4b919.web.app",
+    "https://eventhub-4b919.firebaseapp.com"
+]}})
     
     
     @jwt.expired_token_loader
